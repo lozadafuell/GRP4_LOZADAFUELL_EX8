efa8aa07d968f615ba9150c80a6f7cf25ae9fb8e
Fuell Lozada <lozadafuell@gmail.com>
2024-11-05 14:55:39 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,8 @@
-<!-- logout.php -->
 <?php
 session_start();
 session_unset();
 session_destroy();
 
-// Clear the username cookie
 setcookie('username', '', time() - 3600, '/');
 
 header("Location: index.php?page=home");
